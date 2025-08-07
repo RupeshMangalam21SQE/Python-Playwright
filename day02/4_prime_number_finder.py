70588e07c5d07de456d1d0b1e016a002cd56967f
RupeshMangalam21SQE <rupesh.mangalam@sqelabs.com>
2025-08-07 10:23:31 +0530

    Add all python exercises for Day 02

---
+++ day02/4_prime_number_finder.py
@@ -0,0 +1,12 @@
+start = int(input("Enter start of range: "))
+end = int(input("Enter end of range: "))
+
+print(f"Prime numbers between {start} and {end} are:")
+for num in range(start, end + 1):
+    if num < 2:
+        continue
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            break
+    else:
+        print(num)
