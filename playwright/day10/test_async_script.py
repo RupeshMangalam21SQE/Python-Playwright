5bd1cec5b37e4a90562ef4b16d95ae0be85f18ac
RupeshMangalam21SQE <rupesh.mangalam@sqelabs.com>
2025-09-11 10:24:39 +0530

    move assertion to test function

--- playwright/day10/test_async_script.py
+++ playwright/day10/test_async_script.py
@@ -3,13 +3,14 @@ from playwright.async_api import async_playwright, expect
 
 async def _run_test(page):
     await page.goto("https://playwright.dev/")
-    await expect(page).to_have_title("Fast and reliable end-to-end testing for modern web apps | Playwright")
 
 async def async_main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False)
         page = await browser.new_page()
         await _run_test(page)
+        await expect(page).to_have_title(
+            "Fast and reliable end-to-end testing for modern web apps | Playwright")
         await browser.close()
 
 def test_async_script():
