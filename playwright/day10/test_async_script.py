8ee7e3b0e7d64757c81cc41a0e2dbe6d04c33802
RupeshMangalam21SQE <rupesh.mangalam@sqelabs.com>
2025-09-11 10:27:06 +0530

    improved variable naming to be more readable

--- playwright/day10/test_async_script.py
+++ playwright/day10/test_async_script.py
@@ -1,17 +1,17 @@
 import asyncio
 from playwright.async_api import async_playwright, expect
 
-async def _run_test(page):
+async def _navigate_to_home(page):
     await page.goto("https://playwright.dev/")
 
 async def async_main():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch(headless=False)
-        page = await browser.new_page()
-        await _run_test(page)
-        await expect(page).to_have_title(
+    async with async_playwright() as playwright:
+        chromium_browser = await playwright.chromium.launch(headless=False)
+        homepage = await chromium_browser.new_page()
+        await _navigate_to_home(homepage)
+        await expect(homepage).to_have_title(
             "Fast and reliable end-to-end testing for modern web apps | Playwright")
-        await browser.close()
+        await chromium_browser.close()
 
 def test_async_script():
     asyncio.run(async_main())
